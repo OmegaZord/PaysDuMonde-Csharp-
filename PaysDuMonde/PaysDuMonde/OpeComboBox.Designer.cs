d1e929101b2753e38249b30761487124c3939814
OmegaZord <acharles0111@gmail.com>
2017-09-25 22:30:47 +0200

    Initialisation du projet Pays du monde via combobox (c# - winforms)

---
+++ PaysDuMonde/PaysDuMonde/OpeComboBox.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace OperationComboBox
+{
+    partial class OpeComboBox
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbCultures = new System.Windows.Forms.ListBox();
+            this.cbCultures = new System.Windows.Forms.ComboBox();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnFullRight = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnFullLeft = new System.Windows.Forms.Button();
+            this.btnBottom = new System.Windows.Forms.Button();
+            this.btnTop = new System.Windows.Forms.Button();
+            this.btnLess = new System.Windows.Forms.Button();
+            this.btnMore = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbCultures
+            // 
+            this.lbCultures.FormattingEnabled = true;
+            this.lbCultures.Location = new System.Drawing.Point(282, 13);
+            this.lbCultures.Name = "lbCultures";
+            this.lbCultures.Size = new System.Drawing.Size(279, 277);
+            this.lbCultures.TabIndex = 9;
+            this.lbCultures.SelectedIndexChanged += new System.EventHandler(this.lbCultures_SelectedIndexChanged);
+            // 
+            // cbCultures
+            // 
+            this.cbCultures.FormattingEnabled = true;
+            this.cbCultures.Location = new System.Drawing.Point(26, 13);
+            this.cbCultures.Name = "cbCultures";
+            this.cbCultures.Size = new System.Drawing.Size(121, 21);
+            this.cbCultures.TabIndex = 0;
+            this.cbCultures.SelectedIndexChanged += new System.EventHandler(this.cbCultures_SelectedIndexChanged);
+            this.cbCultures.TextChanged += new System.EventHandler(this.cbCultures_TextChanged);
+            this.cbCultures.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cbCultures_KeyDown);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(183, 103);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 23);
+            this.btnRight.TabIndex = 1;
+            this.btnRight.Text = ">";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnFullRight
+            // 
+            this.btnFullRight.Location = new System.Drawing.Point(182, 161);
+            this.btnFullRight.Name = "btnFullRight";
+            this.btnFullRight.Size = new System.Drawing.Size(75, 23);
+            this.btnFullRight.TabIndex = 3;
+            this.btnFullRight.Text = ">>";
+            this.btnFullRight.UseVisualStyleBackColor = true;
+            this.btnFullRight.Click += new System.EventHandler(this.btnFullRight_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(183, 132);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnLeft.TabIndex = 2;
+            this.btnLeft.Text = "<";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnFullLeft
+            // 
+            this.btnFullLeft.Location = new System.Drawing.Point(182, 190);
+            this.btnFullLeft.Name = "btnFullLeft";
+            this.btnFullLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnFullLeft.TabIndex = 4;
+            this.btnFullLeft.Text = "<<";
+            this.btnFullLeft.UseVisualStyleBackColor = true;
+            this.btnFullLeft.Click += new System.EventHandler(this.btnFullLeft_Click);
+            // 
+            // btnBottom
+            // 
+            this.btnBottom.Location = new System.Drawing.Point(421, 325);
+            this.btnBottom.Name = "btnBottom";
+            this.btnBottom.Size = new System.Drawing.Size(75, 23);
+            this.btnBottom.TabIndex = 8;
+            this.btnBottom.Text = "Bottom";
+            this.btnBottom.UseVisualStyleBackColor = true;
+            this.btnBottom.Click += new System.EventHandler(this.btnBottom_Click);
+            // 
+            // btnTop
+            // 
+            this.btnTop.Location = new System.Drawing.Point(340, 325);
+            this.btnTop.Name = "btnTop";
+            this.btnTop.Size = new System.Drawing.Size(75, 23);
+            this.btnTop.TabIndex = 7;
+            this.btnTop.Text = "Top";
+            this.btnTop.UseVisualStyleBackColor = true;
+            this.btnTop.Click += new System.EventHandler(this.btnTop_Click);
+            // 
+            // btnLess
+            // 
+            this.btnLess.Location = new System.Drawing.Point(340, 296);
+            this.btnLess.Name = "btnLess";
+            this.btnLess.Size = new System.Drawing.Size(75, 23);
+            this.btnLess.TabIndex = 5;
+            this.btnLess.Text = "-";
+            this.btnLess.UseVisualStyleBackColor = true;
+            this.btnLess.Click += new System.EventHandler(this.btnLess_Click);
+            // 
+            // btnMore
+            // 
+            this.btnMore.Location = new System.Drawing.Point(421, 296);
+            this.btnMore.Name = "btnMore";
+            this.btnMore.Size = new System.Drawing.Size(75, 23);
+            this.btnMore.TabIndex = 6;
+            this.btnMore.Text = "+";
+            this.btnMore.UseVisualStyleBackColor = true;
+            this.btnMore.Click += new System.EventHandler(this.btnMore_Click);
+            // 
+            // OpeComboBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 363);
+            this.Controls.Add(this.btnMore);
+            this.Controls.Add(this.btnFullLeft);
+            this.Controls.Add(this.btnLess);
+            this.Controls.Add(this.btnTop);
+            this.Controls.Add(this.btnFullRight);
+            this.Controls.Add(this.btnBottom);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.lbCultures);
+            this.Controls.Add(this.cbCultures);
+            this.Name = "OpeComboBox";
+            this.Text = "ComboBox et ListBox";
+            this.Load += new System.EventHandler(this.OpeComboBox_Load);
+            this.Click += new System.EventHandler(this.OpeComboBox_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbCultures;
+        private System.Windows.Forms.ComboBox cbCultures;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnFullRight;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnFullLeft;
+        private System.Windows.Forms.Button btnBottom;
+        private System.Windows.Forms.Button btnTop;
+        private System.Windows.Forms.Button btnLess;
+        private System.Windows.Forms.Button btnMore;
+    }
+}
+
---
+++ PaysDuMonde/PaysDuMonde/OpeComboBox.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Globalization;
+using System.IO;
+
+namespace OperationComboBox
+{
+    public partial class OpeComboBox : Form
+    {
+        /**********************************
+            Champs / Propriétés / Enums
+         **********************************/
+        #region Fields
+
+        enum SwitchBox
+        {
+            Left,
+            FullLeft,
+            Right,
+            FullRight,
+        }
+        enum MoveList
+        {
+            Up,
+            Down,
+            Top,
+            Bottom,
+        }
+
+        private ErrorProvider _err = new ErrorProvider();
+        private List<string> _listException = new List<string>();
+
+        public List<string> ListException
+        {
+            get { return _listException; }
+            set { _listException = value; }
+        }
+
+        public ErrorProvider Err
+        {
+            get { return _err; }
+            set { _err = value; }
+        }
+
+        #endregion
+
+        /*******************************
+                Constructeurs
+         *******************************/
+
+        #region Ctor
+
+        public OpeComboBox()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        /*******************************
+                Evènements
+         *******************************/
+        #region Events
+
+        private void OpeComboBox_Load(object sender, EventArgs e)
+        {
+
+            foreach (CultureInfo ci in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            {
+                try
+                {
+                    RegionInfo ri = new RegionInfo(ci.LCID);
+                    if (!lbCultures.Items.Contains(ri.DisplayName) && saisieValide(ri.DisplayName))
+                        lbCultures.Items.Add(ri.DisplayName);
+                }
+                catch (Exception ex)
+                {
+                    ListException.Add(ex.Message);
+                }
+            }
+            //foreach (CultureInfo ci in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            //{
+            //    if (!lbCultures.Items.Contains(ci.DisplayName))
+            //        lbCultures.Items.Add(ci.DisplayName);
+            //}
+            //saveFileExceptions();
+            activateButtons();
+        }
+
+        private void cbCultures_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            activateButtons();
+        }
+
+        private void lbCultures_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            activateButtons();
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            switchList(SwitchBox.Right);
+            activateButtons();
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            switchList(SwitchBox.Left);
+            activateButtons();
+        }
+
+        private void btnFullRight_Click(object sender, EventArgs e)
+        {
+            switchList(SwitchBox.FullRight);
+            activateButtons();
+        }
+
+        private void btnFullLeft_Click(object sender, EventArgs e)
+        {
+            switchList(SwitchBox.FullLeft);
+            activateButtons();
+        }
+
+        private void btnMore_Click(object sender, EventArgs e)
+        {
+            moveInList(MoveList.Up);
+            activateButtons();
+        }
+
+        private void btnLess_Click(object sender, EventArgs e)
+        {
+            moveInList(MoveList.Down);
+            activateButtons();
+        }
+
+        private void btnTop_Click(object sender, EventArgs e)
+        {
+            moveInList(MoveList.Top);
+            activateButtons();
+        }
+
+        private void btnBottom_Click(object sender, EventArgs e)
+        {
+            moveInList(MoveList.Bottom);
+            activateButtons();
+        }
+
+        private void cbCultures_TextChanged(object sender, EventArgs e)
+        {
+            Err.Clear();
+        }
+
+        private void cbCultures_KeyDown(object sender, KeyEventArgs e)
+        {
+            Err.Clear();
+            saisirCulture(e);
+        }
+
+        private void OpeComboBox_Click(object sender, EventArgs e)
+        {
+            lbCultures.SelectedIndex = -1;
+        }
+
+        #endregion
+
+        /*******************************
+                Méthodes
+         *******************************/
+        #region Methods
+
+        private void moveInList(MoveList d)
+        {
+            Err.Clear();
+            switch (d)
+            {
+                case MoveList.Up:
+                    if (lbCultures.SelectedIndex > 0)
+                    {
+                        int index = lbCultures.SelectedIndex;
+                        lbCultures.Items.Insert(lbCultures.SelectedIndex - 1, lbCultures.Items[lbCultures.SelectedIndex]);
+                        lbCultures.Items.RemoveAt(lbCultures.SelectedIndex);
+                        lbCultures.SelectedIndex = index - 1;
+                    }
+                    else if (lbCultures.SelectedIndex == 0)
+                    {
+                        initializeError(Err, cbCultures, SystemIcons.Warning);
+                        Err.SetError(lbCultures, "Vous ne pouvez pas aller plus haut, vous êtes déjà en haut de la liste.");
+                    }
+                    break;
+                case MoveList.Down:
+                    if (lbCultures.SelectedIndex < lbCultures.Items.Count - 1)
+                    {
+                        int index = lbCultures.SelectedIndex;
+                        lbCultures.Items.Insert(lbCultures.SelectedIndex + 2, lbCultures.Items[lbCultures.SelectedIndex]);
+                        lbCultures.Items.RemoveAt(lbCultures.SelectedIndex);
+                        lbCultures.SelectedIndex = index + 1;
+                    }
+                    else if (lbCultures.SelectedIndex == lbCultures.Items.Count - 1)
+                    {
+                        initializeError(Err, cbCultures, SystemIcons.Warning);
+                        Err.SetError(lbCultures, "Vous ne pouvez pas aller plus bas, vous êtes déjà au fond de la liste.");
+                    }
+                    break;
+                case MoveList.Top:
+                    lbCultures.Items.Insert(0, lbCultures.Items[lbCultures.SelectedIndex]);
+                    lbCultures.Items.RemoveAt(lbCultures.SelectedIndex);
+                    lbCultures.SelectedIndex = 0;
+                    break;
+                case MoveList.Bottom:
+                    lbCultures.Items.Insert(lbCultures.Items.Count, lbCultures.Items[lbCultures.SelectedIndex]);
+                    lbCultures.Items.RemoveAt(lbCultures.SelectedIndex);
+                    lbCultures.SelectedIndex = lbCultures.Items.Count - 1;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void switchList(SwitchBox d)
+        {
+            switch (d)
+            {
+                case SwitchBox.Left:
+                    int index = lbCultures.SelectedIndex;
+                    cbCultures.Items.Add(lbCultures.Items[lbCultures.SelectedIndex]);
+                    lbCultures.Items.RemoveAt(lbCultures.SelectedIndex);
+                    if (index >= lbCultures.Items.Count)
+                        lbCultures.SelectedIndex = lbCultures.Items.Count - 1;
+                    else
+                        lbCultures.SelectedIndex = index;
+                    break;
+                case SwitchBox.FullLeft:
+                    for (int i = 0; i < lbCultures.Items.Count; i++)
+                    {
+                        cbCultures.Items.Add(lbCultures.Items[i]);
+                    }
+                    lbCultures.Items.Clear();
+                    break;
+                case SwitchBox.Right:
+                    lbCultures.Items.Add(cbCultures.Items[cbCultures.SelectedIndex]);
+                    cbCultures.Items.RemoveAt(cbCultures.SelectedIndex);
+                    cbCultures.Text = string.Empty;
+                    break;
+                case SwitchBox.FullRight:
+                    for (int i = 0; i < cbCultures.Items.Count; i++)
+                    {
+                        lbCultures.Items.Add(cbCultures.Items[i]);
+                    }
+                    cbCultures.Items.Clear();
+                    cbCultures.Text = string.Empty;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void saisirCulture(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (!saisieValide(cbCultures.Text))
+                {
+                    initializeError(Err, cbCultures, SystemIcons.Error);
+                    Err.SetError(cbCultures, "Vous ne pouvez saisir que des caractères alphabétique séparables par un tiret (la saisie ne doit pas commencer par le tiret).");
+                }
+                else
+                {
+                    string culture = string.Empty;
+
+                    if (cbCultures.Text != string.Empty)
+                        culture = string.Format("{0}{1}", cbCultures.Text.Substring(0, 1).ToUpper(), cbCultures.Text.Substring(1, cbCultures.Text.Length - 1).ToLower());
+
+                    if (!lbCultures.Items.Contains(culture) && !cbCultures.Items.Contains(culture) && cbCultures.Text != string.Empty)
+                    {
+                        cbCultures.Items.Add(culture);
+                        cbCultures.Text = string.Empty;
+                        cbCultures.Focus();
+                        activateButtons();
+                    }
+                    else if (cbCultures.Text == string.Empty)
+                    {
+                        initializeError(Err, cbCultures, SystemIcons.Warning);
+                        Err.SetError(cbCultures, "Impossible d'ajouter une entrée vide.");
+                    }
+                    else
+                    {
+                        initializeError(Err, cbCultures, SystemIcons.Information);
+                        Err.SetError(cbCultures, "La saisie existe déjà dans une des listes.");
+                    }
+                }
+            }
+            if (e.KeyCode == Keys.Escape)
+            {
+                cbCultures.Text = string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Initialise un composant ErrorProvider avec le choix de l'icone
+        /// </summary>
+        /// <param name="err">Le composant ErrorProvider à initialiser</param>
+        /// <param name="ctrl">Le controle sur lequel faire apparaitre l'erreur</param>
+        /// <param name="icon">L'icone à afficher</param>
+        private void initializeError(ErrorProvider err, Control ctrl, Icon icon)
+        {
+            err.SetIconAlignment(ctrl, ErrorIconAlignment.MiddleRight);
+            err.SetIconPadding(ctrl, 2);
+            err.Icon = icon;
+            err.BlinkRate = 250;
+            err.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.AlwaysBlink;
+        }
+
+        /// <summary>
+        /// Activation ou Désactivation des boutons en fonction : de la selection, de listes vides ou non
+        /// </summary>
+        private void activateButtons()
+        {
+            /**********************************************************************
+             * Enable ou Disable les boutons qui vont de la gauche vers la droite *
+             **********************************************************************/
+            if (cbCultures.Items.Count == 0 && cbCultures.SelectedItem == null)
+            {
+                btnRight.Enabled = false;
+                btnFullRight.Enabled = false;
+            }
+            else if (cbCultures.Items.Count > 0 && cbCultures.SelectedItem == null)
+            {
+                btnRight.Enabled = false;
+                btnFullRight.Enabled = true;
+            }
+            else
+            {
+                btnRight.Enabled = true;
+                btnFullRight.Enabled = true;
+            }
+
+            /**********************************************************************
+             * Enable ou Disable les boutons qui vont de la droite vers la gauche *
+             **********************************************************************/
+
+            if (lbCultures.Items.Count == 0 && lbCultures.SelectedItem == null)
+            {
+                btnLeft.Enabled = false;
+                btnFullLeft.Enabled = false;
+                btnMore.Enabled = false;
+                btnLess.Enabled = false;
+                btnTop.Enabled = false;
+                btnBottom.Enabled = false;
+            }
+            else if (lbCultures.Items.Count > 0 && lbCultures.SelectedItem == null)
+            {
+                btnLeft.Enabled = false;
+                btnFullLeft.Enabled = true;
+                btnMore.Enabled = false;
+                btnLess.Enabled = false;
+                btnTop.Enabled = false;
+                btnBottom.Enabled = false;
+            }
+            else
+            {
+                btnLeft.Enabled = true;
+                btnFullLeft.Enabled = true;
+                if (lbCultures.Items.Count > 1) // Si la liste comporte plus d'une entrée, on autorise 
+                {
+                    btnMore.Enabled = true;
+                    btnLess.Enabled = true;
+                    btnTop.Enabled = true;
+                    btnBottom.Enabled = true;
+                }
+                else
+                {
+                    btnMore.Enabled = false;
+                    btnLess.Enabled = false;
+                    btnTop.Enabled = false;
+                    btnBottom.Enabled = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        private bool saisieValide(string s)
+        {
+            if (s.Substring(0, 1).Equals("-"))
+            {
+                return false;
+            }
+
+            foreach (char c in s)
+            {
+                if (!char.IsLetter(c) && !char.IsWhiteSpace(c) && c != '-')
+                    return false;
+            }
+            return true;
+        }
+
+        private void saveFileExceptions()
+        {
+            StreamWriter sw = new StreamWriter("./ListException.txt", true, Encoding.UTF8);
+            for (int i = 0; i < ListException.Count; i++)
+            {
+                sw.WriteLine(string.Format("{0} : {1}", DateTime.Now, ListException[i]));
+            }
+            sw.Close();
+        }
+
+        #endregion
+    }
+}
